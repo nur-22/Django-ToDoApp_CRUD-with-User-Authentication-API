e11c0d8ccc1eb7fe107b649fbc28bbf1b676383f
Nur-E-Jannat <nur.e.jannat.akanto@gmail.com>
2024-12-31 03:55:08 +0600

    with API

--- todoapp/settings.py
+++ todoapp/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'todolist.apps.TodolistConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -78,16 +79,16 @@ WSGI_APPLICATION = 'todoapp.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
-#DATABASES = {
-    #'default': {
-        #'ENGINE': 'django.db.backends.sqlite3',
-        #'NAME': BASE_DIR / 'db.sqlite3',
-    #}
-#}
-
 DATABASES = {
-    'default':dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
+    }
 }
+
+#DATABASES = {
+    #'default':dj_database_url.parse(os.environ.get("DATABASE_URL"))
+#}
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
--- todolist/urls.py
+++ todolist/urls.py
@@ -1,14 +1,20 @@
-from django.urls import path
-from .views import Tlist, Tdetail, Tcreate, Tupdate, Tdelete, Userlogin, Userregister
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
+from .views import Tlist, Tdetail, Tcreate, Tupdate, Tdelete, Userlogin, Userregister, TodolistViewSet
 from django.contrib.auth.views import LogoutView
 
+# Django Rest Framework(DRF) Router
+router = DefaultRouter()
+router.register(r'api/tasks', TodolistViewSet, basename='task')
+
 urlpatterns = [
-    path('login/', Userlogin.as_view(), name = 'login'),
-    path('logout/', LogoutView.as_view(next_page='login'), name = 'logout'),
-    path('register/', Userregister.as_view(), name = 'register'),
-    path('', Tlist.as_view(), name = 'alltask'),
-    path('todolist/<int:pk>/', Tdetail.as_view(), name = 'todolist'),
-    path('create/', Tcreate.as_view(), name = 'create'),
-    path('update/<int:pk>/', Tupdate.as_view(), name = 'update'),
-    path('delete/<int:pk>/', Tdelete.as_view(), name = 'delete'),
+    path('login/', Userlogin.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(next_page='login'), name='logout'),
+    path('register/', Userregister.as_view(), name='register'),
+    path('', Tlist.as_view(), name='alltask'),
+    path('todolist/<int:pk>/', Tdetail.as_view(), name='todolist'),
+    path('create/', Tcreate.as_view(), name='create'),
+    path('update/<int:pk>/', Tupdate.as_view(), name='update'),
+    path('delete/<int:pk>/', Tdelete.as_view(), name='delete'),
+    path('', include(router.urls)),  # the router-generated URLs
 ]
